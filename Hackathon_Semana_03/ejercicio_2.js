c6c8a32df10a6e05d94119517940a15a51ca1cc2
Brayan Laureano <blaureano@rcp.pe>
2021-09-11 11:16:19 -0500

    :tada actualizando tarea terminada:

--- Hackathon_Semana_03/ejercicio_2.js
+++ Hackathon_Semana_03/ejercicio_2.js
@@ -5,9 +5,7 @@
 // functionkeys.push("more");
 // ¿Por qué? ¿Cómo podemos arreglar el código para que funcione keys.push?
 
+// Respuesta
 
-let map = new Map();
-map.set("name", "John")
-let keys = map.keys();// Error: keys.push is not a
-// functionkeys.push("more");
-console.log(keys);
+// No se podra solucionar por que el newMap() solo crea objetos de tipo clave y valor, tendriamos que utilizar y array de objetos para crear una matriz de claves y valores.
+// Se puedo solucionar utilizando un bucle for of, pero no es la mejor solucion.
