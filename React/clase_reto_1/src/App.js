1d9bdb7960ac979644a373aa168a6ffb62377757
Brayan Laureano <blaureano@rcp.pe>
2021-09-27 20:09:20 -0500

    :tada: subiendo reto 1

---
+++ React/clase_reto_1/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="box">
+        <div className="group">
+        <label htmlFor="">Full Name</label>
+        <input type="text" value='Brayan Laureano Paucar' />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
